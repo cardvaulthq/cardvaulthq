876489ab5248597a422983137d2948aa45827a3f
cardvaulthq <cardvault88@gmail.com>
2026-02-05 10:46:59 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 export default app;
